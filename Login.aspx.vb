bc4192be0d21b8c1a1157fb984d387ec0fd7ac07
Daniel Rodríguez <drodriguezquesada19@gmail.com>
2025-07-25 14:05:21 -0600

    trabajando en el login

--- Login.aspx.vb
+++ Login.aspx.vb
@@ -1,10 +1,37 @@
-﻿Public Class Login
+﻿Imports System.Data.SqlClient
+
+Public Class Login
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
+    Protected Function VerificarUsuario(email As String, password As String) As Boolean
+        Try
+            Dim helper As New DatabaseHelper()
+            Dim parametros As New List(Of SqlParameter) From {
+                New SqlParameter("@Email", email),
+                New SqlParameter("@Password", password)
+            }
+            ' Aquí se ejecuta la consulta para verificar las credenciales del usuario
+            Dim dataTable As DataTable = helper.ExecuteQuery("SELECT * FROM Usuarios WHERE EMAIL = @Email AND CONTRASEÑA = @Password", parametros)
 
+            If dataTable.Rows.Count > 0 Then
+                ' Usuario encontrado, puedes redirigir o realizar otra acción
+                Session.Add("UsuarioId", dataTable.Rows(0)("Id").ToString())
+                Session.Add("UsuarioNombre", dataTable.Rows(0)("Nombre").ToString())
+                Session.Add("UsuarioApellido", dataTable.Rows(0)("Apellido").ToString())
+                Session.Add("UsuarioEmail", dataTable.Rows(0)("Email").ToString())
+            Else
+                ' Usuario no encontrado, manejar el error
+                Return False
+            End If
+
+        Catch ex As Exception
+            Return False
+        End Try
+
+    End Function
     Protected Sub btnLogin_Click(sender As Object, e As EventArgs)
         Dim email As String = txtEmail.Text
         Dim password As String = txtPass.Text
@@ -14,7 +41,12 @@
         }
         Dim helper As New DatabaseHelper()
         ' Validar el usuario
-
+        Dim parametros As New List(Of SqlParameter) From {
+            New SqlParameter("@Email", email),
+            New SqlParameter("@Password", password)
+        }
+        ' ' Aquí se ejecuta la consulta para verificar las credenciales del usuario
+        Dim dataTable As DataTable = helper.ExecuteQuery("SELECT * FROM Usuarios WHERE EMAIL = @Email AND CONTRASEÑA = @Password", parametros)
 
         If usuario.Validar() Then
             If email = "test@example.com" And password = "password" Then
